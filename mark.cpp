0020018b5772c1b9872d6478658e036014e048ae
smit umraliya <umaraliyasmeet12@gmail.com>
2023-10-03 15:06:44 +0530

    adding 1.cpp file

---
+++ mark.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+class student{
+
+private:
+
+    int maths;
+    int science;
+    int english;
+    int gujrati;
+    int total;
+
+public:
+
+    int m = maths;
+    int s = science;
+    int e = english;
+    int g = gujrati;
+    int t = total;
+
+void std(int m, int s, int e, int g)
+{
+    t = m + s + e + g;
+
+    cout << "total : " << t << endl;
+}
+
+};
+
+int main()
+{
+    student s1[2];
+    int a[2], b[2], c[2], d[2], i;
+
+    for(int i ;i < 2; i++)
+
+    cout << "maths : ";
+    cin >> a[i];
+
+    cout << "science : ";
+    cin >> b[i];
+
+    cout << "english : ";
+    cin >> c[i];
+
+    cout << "gujrati : ";
+    cin >> d[i];
+
+    for(int i ; i < 2; i++)
+    {
+        s1[i].std(a[i] ,b[i] ,c[i] ,d[i]);
+    }
+
+    return 0;
+}
